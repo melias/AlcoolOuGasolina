b16ae3f60b6a953d2bdf0a98c04fe8afc089631c
maykon.elias <maykon.elias@hhsurge.com>
2020-01-09 14:09:39 +0000

    Initial commit

---
+++ app/src/main/java/com/mykoelias/alcoolougasolina/MainActivity.java
@@ -0,0 +1,38 @@
+package com.mykoelias.alcoolougasolina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editPrecoAlcool;
+    private EditText editPrecoGasolina;
+    private TextView textResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editPrecoAlcool = findViewById(R.id.editAlcool);
+        editPrecoGasolina = findViewById(R.id.editGasolina);
+        textResultado = findViewById(R.id.textResultado);
+    }
+
+    public void calculo(View v)
+    {
+        Double precoAlcool = Double.parseDouble(editPrecoAlcool.getText().toString());
+        Double precoGasolina = Double.parseDouble(editPrecoGasolina.getText().toString());
+        Double resultado =  precoAlcool / precoGasolina;
+
+        if (resultado >= 0.7) {
+            textResultado.setText("Melhor usar gasolina");
+        } else {
+            textResultado.setText("Melhor usar alcool");
+        }
+    }
+}
